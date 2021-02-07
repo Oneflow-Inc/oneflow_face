d2692ecfff31f59f6373a32eb2715ecc3431520b
Flowingsun007 <flowingsun007@163.com>
2021-02-07 13:48:15 +0800

    update scripts

--- tools/model_convert/resnet100/of_model_2_mx.sh
+++ tools/model_convert/resnet100/of_model_2_mx.sh
@@ -1,3 +1,7 @@
-python of_model_2_mxnet_model.py --mxnet_load_prefix='../../../models/mxnet/model-r100-ii/model' \
---mxnet_load_epoch=0 --mxnet_save_prefix='../../../models/oneflow2mxnet/model' \
---mxnet_save_epoch=17 --of_model_dir='../../../models/r100-arcface-emore/snapshot_17/'
+python of_model_2_mxnet_model.py \
+--mxnet_load_prefix='../../../models/mxnet/model-r100-ii/model' \
+--mxnet_load_epoch=0 \
+--mxnet_save_prefix='../../../models/oneflow2mxnet/emore_r100_arcface_model' \
+--mxnet_save_epoch=0 \
+--of_model_dir='../../../models/emore_r100_arcface_model/'
+
