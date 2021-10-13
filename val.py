1c6ab5350a3aad874ff584e27e6b0b860d02c08d
olojuwin <502401452@qq.com>
2021-10-13 17:27:23 +0800

    Delete extra blank lines

--- val.py
+++ val.py
@@ -1,21 +1,16 @@
+from utils.utils_logging import AverageMeter, init_logging
+import argparse
+from function import Validator
+from utils.utils_config import get_config
+import logging
+import os
+from backbones import get_model
+from utils.utils_callbacks import CallBackVerification
+from eval import verification
 import oneflow as flow
 
 import sys
 sys.path.append("..")
-from eval import verification
-from utils.utils_callbacks import CallBackVerification
-from backbones import get_model
-import os
-import logging
-from utils.utils_config import get_config
-from function import Validator
-import argparse
-from utils.utils_logging import AverageMeter, init_logging
-
-
-
-
-
 
 
 def main(args):
@@ -24,12 +19,12 @@ def main(args):
 
     logging.basicConfig(level=logging.NOTSET)
     logging.info(args.model_path)
-    val_infer=Validator(cfg)
-    val_callback=CallBackVerification(1,cfg.val_targets,cfg.eval_ofrecord_path,image_nums=cfg.val_image_num)     
-    val_infer.load_checkpoint(args.model_path )
-    
-    val_callback(1000, val_infer.get_symbol_val_fn)
+    val_infer = Validator(cfg)
+    val_callback = CallBackVerification(
+        1, cfg.val_targets, cfg.eval_ofrecord_path, image_nums=cfg.val_image_num)
+    val_infer.load_checkpoint(args.model_path)
 
+    val_callback(1000, val_infer.get_symbol_val_fn)
 
 
 if __name__ == "__main__":
@@ -38,4 +33,3 @@ if __name__ == "__main__":
     parser.add_argument('config', type=str, help='py config file')
     parser.add_argument('--model_path', type=str, help='model path')
     main(parser.parse_args())
-
