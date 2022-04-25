127c04bdf6580179a3920c31c7f1e6dd3576f299
WangYi <buaawangyi03@gmail.com>
2022-04-25 16:20:06 +0800

    add downloading data from oss, add convert mxrecord to ofrecord

--- tests/train/test_train.py
+++ tests/train/test_train.py
@@ -1,18 +1,30 @@
+import shutil
 import sys
+import unittest
+from pathlib import Path
 
 import oneflow as flow
+import oneflow.unittest
 from easydict import EasyDict
 from omegaconf import OmegaConf
 from oneflow.utils.data import DataLoader, TensorDataset
 
 from flowface.train_tools.train_function import Trainer
+from flowface.utils.file_utils import get_data_from_cache
 from flowface.utils.utils_config import get_config
-import unittest
-import oneflow.unittest
+
 sys.path.append(".")
+
 
 class TestTrain(flow.unittest.TestCase):
     def setUp(self) -> None:
+        CI_DATA_URL = "https://oneflow-static.oss-cn-beijing.aliyuncs.com/ci-files/dataset/oneflow_face/ci.zip"
+        CI_DATA_MD5 = "077188cffb008ea9973f64a4eb2f7bdb"
+        CACHE_DIR = Path("./ci_data")
+        if flow.env.get_rank() == 0:
+            get_data_from_cache(CI_DATA_URL, CACHE_DIR, md5=CI_DATA_MD5)
+            shutil.unpack_archive(str(Path(CACHE_DIR) / CI_DATA_URL.split("/")[-1]), CACHE_DIR)
+
         config = get_config()
         config.loss = "cosface"
         config.network = "r18"
@@ -28,7 +40,7 @@ class TestTrain(flow.unittest.TestCase):
         config.batch_size = 128
         config.lr = 0.1  # batch size is 512
 
-        config.ofrecord_path = "/workspace/data/oneflow_face_subset"
+        config.ofrecord_path = CACHE_DIR
         config.ofrecord_part_num = 8
         config.num_classes = 3000
         config.num_image = 3000
@@ -36,7 +48,6 @@ class TestTrain(flow.unittest.TestCase):
         config.warmup_epoch = -1
         config.decay_epoch = [10, 16, 22]
         config.val_targets = ["lfw_subset", "cfp_fp_subset", "agedb_30_subset"]
-        # config.val_targets = ["cfp_fp_subset",]
         config.val_frequence = 1000
         config.channel_last = False
         config.is_global = True
@@ -50,7 +61,7 @@ class TestTrain(flow.unittest.TestCase):
         config.use_gpu_decode = False
         config.is_global = True
         self.cfg = config
-    
+
     # @flow.unittest.skip_unless_1n4d()
     def test_eager_global(self):
         self.cfg.is_global = True
@@ -61,7 +72,7 @@ class TestTrain(flow.unittest.TestCase):
         margin_softmax = flow.nn.CombinedMarginLoss(1, 0.0, 0.4).to("cuda")
         trainer = Trainer(self.cfg, margin_softmax, placement, "", world_size, rank)
         trainer()
-    
+
     def test_graph(self):
         self.cfg.is_global = True
         self.cfg.graph = True
@@ -71,7 +82,6 @@ class TestTrain(flow.unittest.TestCase):
         margin_softmax = flow.nn.CombinedMarginLoss(1, 0.0, 0.4).to("cuda")
         trainer = Trainer(self.cfg, margin_softmax, placement, "", world_size, rank)
         trainer()
-
 
 
 if __name__ == "__main__":
