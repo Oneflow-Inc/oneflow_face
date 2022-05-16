d4067585f49f4396f4b80187a9f593b9f39ac663
WangYi <buaawangyi03@gmail.com>
2022-05-16 16:05:38 +0800

    refactor configs by using OmegaConf, remove easydict

--- flowface/utils/utils_config.py
+++ flowface/utils/utils_config.py
@@ -1,11 +1,13 @@
 import importlib
+import yaml
 import os.path as osp
 import warnings
 from pathlib import Path
+from omegaconf import OmegaConf
 
 
 def get_config(config_file=None):
-    from flowface.configs.base import config as base_config
+    from flowface.configs.base import base_config
     if config_file is None:
         warnings.warn("config_file is None, load base config")
         return base_config
@@ -13,11 +15,14 @@ def get_config(config_file=None):
     config_file = Path(config_file)
     if not config_file.exists():
         raise FileNotFoundError(f"can't find config file {str(config_file)}")
-
-    config_file_module = str(config_file.parent) + "." + config_file.stem
-    config = importlib.import_module(config_file_module)
-    job_cfg = config.config
-    base_config.update(job_cfg)
+    with open(config_file, "r") as f:
+        config = yaml.load(f, yaml.FullLoader)
+    base_config.update(config)
     if base_config.output is None:
         base_config.output = osp.join("work_dirs", config_file.stem)
     return base_config
+
+        
+
+def dump_config(config, file_path):
+    OmegaConf.save(config=config, f=file_path)
