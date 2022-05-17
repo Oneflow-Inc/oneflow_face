5cdeefe989ccc41e95428134ef8e2fb3d8cb81ed
WangYi <buaawangyi03@gmail.com>
2022-05-17 21:08:09 +0800

    remove nonexisted module

--- flowface/utils/__init__.py
+++ flowface/utils/__init__.py
@@ -1,4 +1,3 @@
-from .losses import CrossEntropyLoss_sbp
 from .ofrecord_data_utils import OFRecordDataLoader, SyntheticDataLoader
 from .utils_callbacks import CallBackLogging, CallBackModelCheckpoint, CallBackVerification
 from .utils_config import get_config
