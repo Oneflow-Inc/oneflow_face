6f876bf5bcdc7a6ec6157ce9be8ad9a195882839
WangYi <buaawangyi03@gmail.com>
2022-06-23 13:40:33 +0800

    remove redundant file, add save_frequent param

--- flowface/configs/baseline.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from omegaconf import OmegaConf
-
-baseline_config_file = Path(__file__).parent / "baseline.yaml"
-baseline_config = OmegaConf.load(baseline_config_file)
-baseline_config.ckpt_path = str((Path(__file__).parent.parent / "checkpoints").resolve())
